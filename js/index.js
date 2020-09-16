c7811f842defc3f5f246c4526b8319ea7cb61e44
Lukas Mo <67667042+adammolina@users.noreply.github.com>
2020-09-16 01:43:47 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 let button = document.querySelector('button');
-let attribute = document.querySelector('audio').getAttribute('muted');
 
 button.onclick = () => {
-  if (attribute == false) {
-    document.querySelector("audio").setAttribute("muted","true");
-  } else {
-    document.querySelector("audio").setAttribute("muted","false");
-  }
+  document.querySelector('audio').setAttribute("muted", "false");
 };
