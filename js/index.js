8a1e9f73b7394ca31d749c358dff911d83d4bf79
Lukas Mo <67667042+adammolina@users.noreply.github.com>
2020-09-16 20:30:50 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
+window.addEventListener('load', (event) => {
+  var audioTag = document.queryselector("audio");
+  audioTag.setAttribute("muted", "true");
+});
+
 var a = document.getElementById("lofiNoise");
 
-        function toggleAudio() {
-            a.muted = true;
-        }
+function toggleAudio() {
+  if (a.muted = true) {
+    a.muted = false;
+  } else {
+    a.setAttribute("muted", "true");
+  }
+}
